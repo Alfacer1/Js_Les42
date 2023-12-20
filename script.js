ac6b50f8b383b2d330340ff1c5014b39a547251c
Atom <108768353+Alfacer1@users.noreply.github.com>
2023-12-20 19:05:34 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const camelCaseToKebabCase = (string) => {
     return string.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase();
   }
   
-  console.log(camelCaseToKebabCase('myClass')); // 'my-class'
-  console.log(camelCaseToKebabCase('getElementById')); // 'get-element-by-id'
-  console.log(camelCaseToKebabCase('class')); // 'class'
+  console.log(camelCaseToKebabCase('myClass'));
+  console.log(camelCaseToKebabCase('getElementById'));
+  console.log(camelCaseToKebabCase('class'));
   
